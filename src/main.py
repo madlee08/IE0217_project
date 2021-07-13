56ab08511e969b0331d76e0bd64321718c4fa0e0
madlee08 <maikemai12@hotmail.com>
2021-07-13 15:05:16 -0600

    Cambios en las clases importar
    Se implementaron cambios en la clase
    importar_xlsx (renombrado a importar_excel)
    para que acepte mas de una extension (xls,
    xlsm, xlsb, etc)

--- src/main.py
+++ src/main.py
@@ -3,25 +3,19 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-class importar_arxiv_pd:
+class importar_arxiv:
     def __init__(self, ruta_de_archivo):
         if not ruta_de_archivo.endswith(self.extension):
             raise Exception('Formato incorrecto')
         self.ruta = ruta_de_archivo
 
-class importar_xlsx(importar_arxiv_pd):
-    extension = 'xlsx'
-
-    def importar(self):
-        return pd.read_excel(self.ruta)
-
-class importar_csv(importar_arxiv_pd):
+class importar_csv(importar_arxiv):
     extension = 'csv'
 
     def importar(self):
         return pd.read_csv(self.ruta)
 
-class importar_arxiv_gpd:
+class importar_arxivs:
     def __init__(self, ruta_de_archivo):
         valido = False
         for extension in self.extensiones:
@@ -32,7 +26,13 @@ class importar_arxiv_gpd:
             raise Exception('Formato incorrecto')
         self.ruta = ruta_de_archivo
 
-class importar_geodata(importar_arxiv_gpd):
+class importar_excel(importar_arxivs):
+    extensiones = ['xls', 'xlsx', 'xlsm', 'xlsb', 'odf', 'odt']
+
+    def importar(self, hoja=0):
+        return pd.read_excel(self.ruta, sheet_name=hoja)
+
+class importar_geodata(importar_arxivs):
     extensiones = ['shp', 'gpkg', 'geojson']
 
     def importar(self):
@@ -45,3 +45,7 @@ print(DF)
 geodata = importar_geodata('../Datos/costa_rica_cantones.geojson')
 GDF = geodata.importar()
 print(GDF)
+
+data_excel = importar_excel('../Datos/07_12_21_EXCEL_SERIES.xlsx')
+DF_excel = data_excel.importar(hoja='2_5 CANT_NUEVOS')
+print(DF_excel)
